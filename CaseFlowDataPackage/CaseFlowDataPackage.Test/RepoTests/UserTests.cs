d668b5fc07bc5ef655c36d043641ae88cb11c640
Abdul Naqshband <imotionsoftware@hotmail.com>
2025-09-25 00:42:54 +0100

    Closes #2 - Create skeleton for Caseflow data package

---
+++ CaseFlowDataPackage/CaseFlowDataPackage.Test/RepoTests/UserTests.cs
@@ -0,0 +1,23 @@
+﻿namespace IMotionSoftware.CaseFlowDataPackage.Test.RepoTests
+{
+    [TestClass]
+    public sealed class UserTests
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            // This method is called before each test method.
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // This method is called after each test method.
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
